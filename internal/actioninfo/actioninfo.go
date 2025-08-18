f7319ee07b513c0610975e0e68d46d392a4843c6
Slonaviator <slonaviator@yandex.ru>
2025-08-18 16:44:34 +0300

    first commit

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,28 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
 	// TODO: добавить методы
+	Parse(string) (err error)
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
 	// TODO: реализовать функцию
+	for _, data := range dataset {
+
+		if err := dp.Parse(data); err != nil {
+			log.Printf("data parsing error: %v", err)
+			continue // Переходим к следующей итерации
+		}
+		if info, err := dp.ActionInfo(); err != nil {
+			log.Printf("error receiving activity information: %v", err)
+		} else {
+			fmt.Println(info)
+		}
+	}
 }
--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,6 +1,7 @@
 package spentenergy
 
 import (
+	"errors"
 	"time"
 )
 
@@ -12,18 +13,57 @@ const (
 	walkingCaloriesCoefficient = 0.5  // коэффициент для расчета калорий при ходьбе.
 )
 
+// Функция рассчитывает количество калорий по входным данным:
+//
+//	кол-во шагов, вес, рост, время активности
+//
+// Возвращает: кол-во калорий (float64) при ходьбе, ошибку
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if duration <= 0 {
+		return 0, errors.New("WalkingSpentCalories: incorrect time")
+	} else if steps <= 0 || height <= 0 || weight <= 0 {
+		return 0, errors.New("walkingSpentCalories: steps, weight, height <= 0")
+	}
+
+	averageSpeed := MeanSpeed(steps, height, duration)
+	numberCal := weight * averageSpeed * duration.Minutes() / float64(minInH)
+	return numberCal * walkingCaloriesCoefficient, nil
 }
 
+// Функция рассчитывает количество калорий по входным данным:
+//
+//	кол-во шагов, вес, рост, время активности
+//
+// Возвращает: кол-во калорий (float64) при беге, ошибку
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if duration <= 0 {
+		return 0, errors.New("RunningSpentCalories: incorrect time")
+	} else if steps <= 0 || height <= 0 || weight <= 0 {
+		return 0, errors.New("runningSpentCalories: steps, weight, height <= 0")
+	}
+
+	averageSpeed := MeanSpeed(steps, height, duration)
+	return weight * averageSpeed * duration.Minutes() / float64(minInH), nil
 }
 
+// Функция принимает количество шагов steps,
+//
+//	рост пользователя height и продолжительность активности duration
+//	и возвращает среднюю скорость.
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
 	// TODO: реализовать функцию
+	if duration.Seconds() <= 0 {
+		return 0
+	}
+	return Distance(steps, height) / duration.Hours()
 }
 
+// Функция принимает количество шагов и рост пользователя в метрах,
+//
+//	а возвращает дистанцию в километрах(float64).
 func Distance(steps int, height float64) float64 {
 	// TODO: реализовать функцию
+	return height * stepLengthCoefficient * float64(steps) / float64(mInKm)
 }
